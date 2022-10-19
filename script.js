c97266e8310fd6d7078308995dbfb710d5847559
Dylan Murphy <dylan_murphy@live.com>
2022-10-19 19:56:58 -0400

    change wait message

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function clickMe() {
   const catFact = document.getElementById("cat-fact");
-  catFact.innerHTML = "getting a cat fact...";
+  catFact.innerHTML = "getting a cat fact... hang in there!";
 
   let response = await fetch("https://meowfacts.herokuapp.com");
   let result = await response.json();
