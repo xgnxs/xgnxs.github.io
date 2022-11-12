6ee9b2776f25aa0cabf84ec5f68bce8e219b88e0
Dylan Murphy <dylan_murphy@live.com>
2022-11-12 11:11:57 -0500

    add coinflip

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-async function clickMe() {
+async function fetchCatFact() {
+  const fetchFactButton = document.getElementById("fetch-fact-button");
+  fetchFactButton.disabled = true;
+
   const catFact = document.getElementById("cat-fact");
   catFact.innerHTML = "getting a cat fact... hang in there!";
 
-  let response = await fetch("https://meowfacts.herokuapp.com");
-  let result = await response.json();
+  const response = await fetch("https://meowfacts.herokuapp.com");
+  const result = await response.json();
   for (const item of result.data) {
     catFact.innerHTML = item;
   }
+
+  fetchFactButton.disabled = false;
+}
+
+async function coinFlip() {
+  const flipButton = document.getElementById("flip-button");
+  flipButton.disabled = true;
+
+  const coinFlip = document.getElementById("coin-flip");
+  coinFlip.innerHTML = "flipping...";
+
+  await new Promise((r) => setTimeout(r, 500));
+
+  const isHeads = Math.floor(Math.random() * 10000) % 2 === 0;
+  coinFlip.innerHTML = isHeads ? "Heads!" : "Tails!";
+
+  flipButton.disabled = false;
 }
